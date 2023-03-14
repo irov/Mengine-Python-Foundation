cf93be842a05b1daf8aa603ec22e1f800f8d9feb
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-03-14 11:38:13 +0100

    reformat + remove passes

--- Scripts/Foundation/DatabaseManager.py
+++ Scripts/Foundation/DatabaseManager.py
@@ -1,5 +1,6 @@
 from Foundation.Database import Database
 
+
 class DatabaseManager(object):
     s_cache_databases = {}
     s_cache_enable = False
@@ -7,13 +8,11 @@ class DatabaseManager(object):
     @staticmethod
     def onInitialize():
         DatabaseManager.s_cache_enable = Mengine.getConfigBool("Python", "DatabaseCache", False)
-        pass
 
     @staticmethod
     def onFinalize():
         DatabaseManager.s_cache_databases = {}
         DatabaseManager.s_cache_enable = False
-        pass
 
     @staticmethod
     def importDatabase(module, name):
@@ -25,14 +24,10 @@ class DatabaseManager(object):
             Module = __import__(ModuleName, fromlist=[FromName])
         except ImportError as ex:
             Trace.log("Manager", 0, "DatabaseManager.importDatabase: %s:%s invalid import %s" % (module, name, ex))
-
             return None
-            pass
 
         DatabaseType = getattr(Module, Name)
-
         return DatabaseType
-        pass
 
     @staticmethod
     def addExternalDatabaseFromRecords(name, *records):
@@ -40,7 +35,6 @@ class DatabaseManager(object):
             return
 
         database = DatabaseManager.addORMsFromRecords(name, records)
-
         DatabaseManager.s_cache_databases[name] = database
 
     @staticmethod
@@ -49,12 +43,7 @@ class DatabaseManager(object):
             for key, value in record.iteritems():
                 setattr(self, key, value)
 
-        def _get(self, name):
-            return None
-
-        # RecordType = type("Record{}".format(name), (), dict(__init__=_record_init, __getattr__=_get))
         RecordType = type("Record{}".format(name), (), dict(__init__=_record_init))
-
         newDatabase = Database()
 
         for record in records:
@@ -67,22 +56,17 @@ class DatabaseManager(object):
     def getDatabase(module, name):
         if name in DatabaseManager.s_cache_databases:
             return DatabaseManager.s_cache_databases[name]
-            pass
 
         Type = DatabaseManager.importDatabase(module, name)
-
         if Type is None:
             return None
-            pass
 
         Database = Type()
 
         if DatabaseManager.s_cache_enable is True:
             DatabaseManager.s_cache_databases[name] = Database
-            pass
 
         return Database
-        pass
 
     @staticmethod
     def getDatabaseRecords(module, param):
@@ -90,14 +74,10 @@ class DatabaseManager(object):
 
         if database is None:
             Trace.log("Manager", 0, "DatabaseManager.getDatabaseRecords: invalid param %s" % (param))
-
             return None
-            pass
 
         records = database.getRecords()
-
         return records
-        pass
 
     @staticmethod
     def getDatabaseRecordsFilterBy(module, param, **keys):
@@ -118,34 +98,24 @@ class DatabaseManager(object):
 
         if database is None:
             Trace.log("Manager", 0, "DatabaseManager.getDatabaseRecords: invalid param %s" % (param))
-
             return None
-            pass
 
         ORMs = database.getORMs()
-
         return ORMs
-        pass
 
     @staticmethod
     def findDatabaseORM(module, param, **keys):
         ORMs = DatabaseManager.getDatabaseORMs(module, param)
-
         if ORMs == []:
             return None
-            pass
 
         ORM = DatabaseManager.find(ORMs, **keys)
-
         return ORM
-        pass
 
     @staticmethod
     def filterDatabaseORM(module, param, filter):
         ORMs = DatabaseManager.getDatabaseORMs(module, param)
-
         return [orm for orm in ORMs if filter(orm) is True]
-        pass
 
     @staticmethod
     def validDatabaseORMs(param, *legends):
@@ -155,7 +125,6 @@ class DatabaseManager(object):
         if database is None:
             Trace.log("Manager", 0, "DatabaseManager.validDatabaseORMs: invalid param %s" % (param))
             return []
-            pass
 
         database_legends = database.getLegends()
 
@@ -164,25 +133,20 @@ class DatabaseManager(object):
             if key not in legends:
                 Trace.log("Manager", 0, "DatabaseManager.validDatabaseORMs: database %s has over param %s" % (param, key))
                 successful = False
-                pass
 
             legends.remove(key)
-            pass
 
         for key in legends:
             Trace.log("Manager", 0, "DatabaseManager.validDatabaseORMs: database %s has missing param %s" % (param, key))
             successful = False
-            pass
 
         return successful
-        pass
 
     @staticmethod
     def getIndexer(orm, master, **keys):
         orm_filter = DatabaseManager.select(orm, **keys)
         indexer = {getattr(v, master): v for v in orm_filter}
         return indexer
-        pass
 
     @staticmethod
     def __equal(record, keys):
@@ -192,16 +156,11 @@ class DatabaseManager(object):
             if record_key_value is None:
                 if hasattr(record, key) is False:
                     return False
-                    pass
-                pass
 
             if record_key_value != value:
                 return False
-                pass
-            pass
 
         return True
-        pass
 
     @staticmethod
     def find(orm, **keys):
@@ -209,6 +168,7 @@ class DatabaseManager(object):
 
         for record in orm:
             successful = True
+
             for key, value in keys_items:
                 record_key_value = getattr(record, key, None)
 
@@ -216,26 +176,17 @@ class DatabaseManager(object):
                     if hasattr(record, key) is False:
                         successful = False
                         break
-                        pass
-                    pass
 
                 if record_key_value != value:
                     successful = False
                     break
-                    pass
 
                 successful = True
-                pass
 
             if successful is False:
                 continue
-                pass
 
             return record
-            pass
-
-        return None
-        pass
 
     @staticmethod
     def findDB(db, **keys):
@@ -244,22 +195,19 @@ class DatabaseManager(object):
         if indexer is None:
             orms = db.getORMs()
             return DatabaseManager.find(orms, **keys)
-            pass
+
         elif indexer in keys:
             index = keys[indexer]
-            orms = db.selectIndexerORMs(index)
 
+            orms = db.selectIndexerORMs(index)
             if orms is None:
                 return None
-                pass
 
             return DatabaseManager.find(orms, **keys)
-            pass
+
         else:
             orms = db.getORMs()
             return DatabaseManager.find(orms, **keys)
-            pass
-        pass
 
     @staticmethod
     def getUniqueORMValues(orm, key):
@@ -269,15 +217,12 @@ class DatabaseManager(object):
                 continue
 
             value = getattr(record, key)
-
             if value in unique_values:
                 continue
 
             unique_values.append(value)
-            pass
 
         return unique_values
-        pass
 
     @staticmethod
     def getLowValueFromUniques(orm, key, value):
@@ -297,17 +242,13 @@ class DatabaseManager(object):
         for key, value in keys.iteritems():
             if isinstance(value, list) is True:
                 optionals[key] = value[0]
-                pass
             else:
                 required[key] = value
-                pass
-            pass
 
         orm_required = DatabaseManager.select(orm, **required)
 
         if len(orm_required) == 0:
             return None
-            pass
 
         orm_optionals = DatabaseManager.find(orm_required, **optionals)
 
@@ -316,21 +257,14 @@ class DatabaseManager(object):
                 for key in optionals:
                     if hasattr(record, key) is False:
                         return record
-                        pass
 
                     record_value = getattr(record, key)
-
                     if record_value is None:
                         return record
-                        pass
-                    pass
-                pass
 
             return None
-            pass
 
         return orm_optionals
-        pass
 
     @staticmethod
     def select(orm, **keys):
@@ -340,13 +274,9 @@ class DatabaseManager(object):
         for record in orm:
             if DatabaseManager.__equal(record, keys_items) is False:
                 continue
-                pass
-
             result.append(record)
-            pass
 
         return result
-        pass
 
     @staticmethod
     def unselect(orm, **keys):
@@ -354,13 +284,9 @@ class DatabaseManager(object):
         for record in orm:
             if DatabaseManager.__equal(record, keys) is True:
                 continue
-                pass
-
             result.append(record)
-            pass
 
         return result
-        pass
 
     @staticmethod
     def count(orm, **keys):
@@ -370,13 +296,9 @@ class DatabaseManager(object):
         for record in orm:
             if DatabaseManager.__equal(record, keys_items) is False:
                 continue
-                pass
-
             result += 1
-            pass
 
         return result
-        pass
 
     @staticmethod
     def exist(orm, **keys):
@@ -385,13 +307,9 @@ class DatabaseManager(object):
         for record in orm:
             if DatabaseManager.__equal(record, keys_items) is False:
                 continue
-                pass
-
             return True
-            pass
 
         return False
-        pass
 
     @staticmethod
     def getValueFromEntityDefaults(key, entity, tag=None):
@@ -403,4 +321,3 @@ class DatabaseManager(object):
             value = DatabaseManager.find(defaultORMs, Entity=entity, Key=key, Tag=tag).Value
 
         return value
-    pass
