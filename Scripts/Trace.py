8c1f6cd81cef8a073abdc8f7f53097e6e2b28e7e
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-03-16 16:20:57 +0100

    reformat

--- Scripts/Trace.py
+++ Scripts/Trace.py
@@ -1,15 +1,14 @@
 import sys
 import traceback
-
 from TraceManager import TraceManager
+
 
 def msg(text, *args):
     try:
         print(text % args)
     except Exception as ex:
         print(text, args, ex)
-        pass
-    pass
+
 
 def msg_err(text, *args):
     if _PYTHON_VERSION < 300:
@@ -17,23 +16,17 @@ def msg_err(text, *args):
             print >> sys.stderr, text % args
         except Exception as ex:
             print >> sys.stderr, text, args, ex
-            pass
-        pass
     else:
         try:
             print(text % args)
         except Exception as ex:
             print(text, args, ex)
-            pass
-        pass
-    pass
+
 
 def log(type, level, text, *args):
     if TraceManager.existIn(type) is False:
         print("trace log no type %s" % (type))
-
         return
-        pass
 
     if level <= TraceManager.getLevel(type):
         if _PYTHON_VERSION < 300:
@@ -45,45 +38,35 @@ def log(type, level, text, *args):
                 print >> sys.stderr, text % args
             except Exception as ex:
                 print >> sys.stderr, text, args, ex
-                pass
-            pass
         else:
             try:
                 print(text % args)
             except Exception as ex:
                 print(text, args, ex)
-                pass
-            pass
 
         if level == 0:
             if _PYTHON_VERSION < 300:
                 print >> sys.stderr, "-----------------------------------------------"
                 print >> sys.stderr, "Trace:"
                 print >> sys.stderr, "-----------------------------------------------"
-                pass
 
             traceback.print_stack()
-            pass
-        pass
 
-    pass
 
 def trace():
     print >> sys.stderr, "-----------------------------------------------"
     print >> sys.stderr, "Trace:"
     print >> sys.stderr, "-----------------------------------------------"
     traceback.print_stack()
-    pass
+
 
 def caller(deep=0):
     frame = sys._getframe()
 
     for index in range(2 + deep):
         frame = frame.f_back
-        pass
 
     code = frame.f_code
     info = (code.co_filename, frame.f_lineno)
 
     return info
-    pass
