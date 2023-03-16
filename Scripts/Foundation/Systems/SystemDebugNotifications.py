88588ee529216e099ec633139d946c359c37829b
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-03-16 16:29:22 +0100

    improve msg formatting
    https://wonderland-games.atlassian.net/browse/HO2-724
    - if there is too much %s - the extra ones will be replaced with “_”
    - if %s is missing - the missing %s is added to the end of the message separated by commas
    - if kwargs - then a formatted dictionary will be added to the end of the message

--- Scripts/Foundation/Systems/SystemDebugNotifications.py
+++ Scripts/Foundation/Systems/SystemDebugNotifications.py
@@ -2,6 +2,7 @@ from Foundation.DebugNotificationsManager import DebugNotificationsManager
 from Foundation.System import System
 from Foundation.Systems.SystemQALogger import SystemQALogger
 
+
 class SystemDebugNotifications(System):
     # Confluence doc: https://wonderland-games.atlassian.net/wiki/spaces/SCR/pages/1772879886/DebugNotifications
 
@@ -44,26 +45,15 @@ class SystemDebugNotifications(System):
                     return str(arg)
 
             args = tuple(map(mapper, args))  # fix for unicode values
-            f_args = " {}".format(args) if show_args is True else ""
-            f_msg = ""
+            kwargs = dict(map(lambda pair: (pair[0], mapper(pair[1])), kwargs.items()))
 
-            if message is not None:
-                f_arg_count = message.count("%s")
-                if f_arg_count != 0:
-                    if len(args) == f_arg_count:
-                        f_msg = message % args
-                    else:
-                        Trace.log("System", 0, "Invalid number of '%%s' ({}) for format {!r} message (got {} args: {})".format(f_arg_count, identity, len(args), list(args)))
-                        if len(args) > message.count("%s"):
-                            _msg = message + " | other args: " + "'%s', " * abs(len(args) - f_arg_count)
-                            f_msg = _msg % args
-                        else:
-                            # todo: replace unnecessary "%s" from message
-                            f_msg = message.replace("%s", "_")
+            f_args = " {}".format(args) if show_args is True else ""
+            f_msg = self._prepareMessageArgs(identity, message, args)
 
             f_message = identity
             f_message += f_args if len(args) > 0 else ""
             f_message += (" | " + f_msg) if f_msg else ""
+            f_message += " | kwargs: {}".format(kwargs) if len(kwargs) > 0 else ""
 
             if _QUALITYASSURANCE is False:
                 Trace.msg("    * " + f_message)
@@ -138,3 +128,36 @@ class SystemDebugNotifications(System):
 
         for observer in SystemDebugNotifications._dev_to_debug_observers:
             Notification.removeObserver(observer)
+
+    # utils
+
+    def _prepareMessageArgs(self, identity, message, args):
+        if message is None:
+            return ""
+
+        s_counts = message.count("%s")
+        if s_counts == 0:
+            return message
+
+        args_count = len(args)
+        if args_count == s_counts:
+            # CASE 1: all ok, message '%s' count and args count are same
+            f_msg = message % args
+        else:
+            Trace.log("System", 3, "Invalid number of '%%s' ({}) for format {!r} message (got {} args: {})"
+                      .format(s_counts, identity, args_count, list(args)))
+
+            # CASE 2: add missing '%s' to the message text
+            if args_count > s_counts:
+                _msg = message + " | other args: " + ", ".join(["'%s'"] * (args_count - s_counts))
+
+            # CASE 2: remove extra '%s' from text
+            else:   # args_count < s_counts
+                to_remove = s_counts - args_count
+                _msg = Utils.replace_last(message, "s%", "_", to_remove, already_reverted_substrings=True)
+                print _msg
+
+            f_msg = _msg % args
+
+        return f_msg
+
