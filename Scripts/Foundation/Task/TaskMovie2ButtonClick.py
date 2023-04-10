2e6c0cce3a9a5fe6d20295503a3eed377304185b
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-04-10 14:07:59 +0200

    add isDown param

--- Scripts/Foundation/Task/TaskMovie2ButtonClick.py
+++ Scripts/Foundation/Task/TaskMovie2ButtonClick.py
@@ -3,23 +3,23 @@ from Foundation.Task.MixinObjectTemplate import MixinMovie2Button
 from Foundation.Task.MixinObserver import MixinObserver
 from Foundation.Task.Task import Task
 
+
 class TaskMovie2ButtonClick(MixinMovie2Button, MixinObserver, Task):
     def _onParams(self, params):
         super(TaskMovie2ButtonClick, self)._onParams(params)
-        pass
+        self.isDown = params.get("isDown", False)
 
     def _onRun(self):
-        self.addObserverFilter(Notificator.onMovie2ButtonClick, self._onMovie2ButtonClick, self.Movie2Button)
+        if self.isDown is False:
+            # if you pressed (isDown=True) but remove arrow from button - it would be Leave state, not Click
+            self.addObserverFilter(Notificator.onMovie2ButtonClick, self._onMovie2ButtonClick, self.Movie2Button)
+        else:
+            self.addObserverFilter(Notificator.onMovie2ButtonPressed, self._onMovie2ButtonClick, self.Movie2Button)
 
         return False
-        pass
 
     def _onMovie2ButtonClick(self, movie2Button):
         if ArrowManager.emptyArrowAttach() is False:
             return False
-            pass
 
         return True
-        pass
-
-    pass
