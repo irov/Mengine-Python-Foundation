19e0280d5efa36af265acc919b4541963869aa36
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-04-24 19:30:06 +0200

    add setProvider, removeProvider

--- Scripts/Foundation/ProviderManager.py
+++ Scripts/Foundation/ProviderManager.py
@@ -30,6 +30,28 @@ class ProviderManager(object):
         return name in ProviderManager.s_providers
 
     @staticmethod
+    def setProvider(name, provider_name, methods):
+        provider = ProviderManager.getProvider(name)
+
+        if provider is None:
+            Trace.log("Manager", 0, "Not found provider {!r}".format(name))
+            return False
+
+        provider.setProvider(provider_name, methods)
+        return True
+
+    @staticmethod
+    def removeProvider(name):
+        provider = ProviderManager.getProvider(name)
+
+        if provider is None:
+            Trace.log("Manager", 0, "Not found provider {!r}".format(name))
+            return False
+
+        provider.removeProvider()
+        return True
+
+    @staticmethod
     def onFinalize():
         for provider in ProviderManager.s_providers.values():
             provider.removeProvider()
