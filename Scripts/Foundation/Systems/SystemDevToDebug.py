b448bf8cd174f7382c292eb4f98ff9ad90e8f6e0
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-05-11 15:07:24 +0200

    fix findWidget

--- Scripts/Foundation/Systems/SystemDevToDebug.py
+++ Scripts/Foundation/Systems/SystemDevToDebug.py
@@ -160,7 +160,7 @@ class SystemDevToDebug(System):
         widgets.append(w_affector)
 
         for widget in widgets:
-            if tab.findWidget(widget) is not None:
+            if tab.findWidget(widget.getId()) is not None:
                 continue
             tab.addWidget(widget)
 
