f175a28e3b98311df109e4a3b40d1d3fcc8399ea
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-08-03 22:39:31 +0300

    hide debug log

--- Scripts/TraceManager.py
+++ Scripts/TraceManager.py
@@ -1,65 +1,52 @@
 class TraceManager(object):
+
     s_traceConfig = {}
     s_levelRange = range(5)
 
     @staticmethod
     def setDictionary(config):
         TraceManager.s_traceConfig = config
-        pass
 
     @staticmethod
     def existIn(type):
         if type in TraceManager.s_traceConfig:
             return True
-            pass
 
         return False
-        pass
 
     @staticmethod
     def addTraces(names):
         for name in names:
             TraceManager.addTrace(name)
-            pass
-        pass
 
     @staticmethod
     def addTrace(name):
         if name in TraceManager.s_traceConfig:
-            print("TraceManager.addType-> TypeName - '%s' already exist in TraceManager.s_traceConfig" % (name))
+            Trace.msg_dev("TraceManager.addType-> TypeName - '%s' already exist in TraceManager.s_traceConfig" % (name))
             return
-            pass
 
         TraceManager.s_traceConfig[name] = 0
-        pass
 
     @staticmethod
     def getLevel(name):
         if name not in TraceManager.s_traceConfig:
-            print("TraceManager.getLevel-> TypeName - '%s' not in TraceManager.s_traceConfig" % (name))
+            Trace.msg_dev("TraceManager.getLevel-> TypeName - '%s' not in TraceManager.s_traceConfig" % (name))
             return
-            pass
 
         return TraceManager.s_traceConfig[name]
-        pass
 
     @staticmethod
     def setLevel(name, levelValue):
         if name not in TraceManager.s_traceConfig:
-            print("TraceManager.setLevel-> TypeName - '%s' not in TraceManager.s_traceConfig" % (name))
+            Trace.msg_dev("TraceManager.setLevel-> TypeName - '%s' not in TraceManager.s_traceConfig" % (name))
             return
-            pass
 
         if levelValue not in TraceManager.s_levelRange:
-            print("TraceManager.setLevel-> LevelValue - '%s' not in TraceManager.s_levelRange" % (levelValue))
+            Trace.msg_dev("TraceManager.setLevel-> LevelValue - '%s' not in TraceManager.s_levelRange" % (levelValue))
             return
-            pass
 
         TraceManager.s_traceConfig[name] = levelValue
-        pass
 
     @staticmethod
     def removeTrace(name):
         TraceManager.s_traceConfig.pop(name)
-        pass
-    pass
