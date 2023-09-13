26542b056c42298800e00343d459ed6a85c2c192
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-09-13 15:17:35 +0300

    fix

--- Scripts/Foundation/GuardMovie2BlockInput.py
+++ Scripts/Foundation/GuardMovie2BlockInput.py
@@ -10,16 +10,20 @@ class GuardMovie2BlockInput(object):
         self.GroupName = GroupName
         self.Movie2Name = Movie2Name
 
-    def blockInput(self, value):
-        movie = GroupManager.getObject(self.GroupName, self.Movie2Name)
+    @staticmethod
+    def blockInput(value, GroupName="BlockInput", Movie2Name="Movie2_BlockInput"):
+        movie = GroupManager.getObject(GroupName, Movie2Name)
         movie.setEnable(value)
+
+    def __blockInput(self, value):
+        self.blockInput(value, self.GroupName, self.Movie2Name)
 
     def __enter__(self):
         if GroupManager.hasObject(self.GroupName, self.Movie2Name) is False:
             Trace.log("Object", 0, "GuardMovie2BlockInput invalid get object %s:%s" % (self.GroupName, self.Movie2Name))
             return self.source
 
-        return self.source.makeGuardSource(self.enable, self.blockInput)
+        return self.source.makeGuardSource(self.enable, self.__blockInput)
 
     def __exit__(self, type, value, traceback):
         if type is not None:
