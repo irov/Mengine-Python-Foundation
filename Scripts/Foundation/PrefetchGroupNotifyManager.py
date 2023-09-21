8ce5ab96b5b4a1d11c7b0ae4f758207f22e9a8ca
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-09-21 18:35:02 +0300

    fix

--- Scripts/Foundation/PrefetchGroupNotifyManager.py
+++ Scripts/Foundation/PrefetchGroupNotifyManager.py
@@ -1,6 +1,7 @@
 from Foundation.DatabaseManager import DatabaseManager
 from Notification import Notification
 
+
 class PrefetchGroupNotifyManager(object):
     STATUS_NO = 0
     STATUS_START = 1
@@ -15,12 +16,11 @@ class PrefetchGroupNotifyManager(object):
     def onInitialize():
         PrefetchGroupNotifyManager.s_onInitializeRenderResourcesObserver = Notification.addObserver(Notificator.onInitializeRenderResources, PrefetchGroupNotifyManager.__onInitializeRenderResources)
         PrefetchGroupNotifyManager.s_onFinalizeRenderResourcesObserver = Notification.addObserver(Notificator.onFinalizeRenderResources, PrefetchGroupNotifyManager.__onFinalizeRenderResources)
-        pass
 
     @staticmethod
     def onFinalize():
         Notification.removeObserver(PrefetchGroupNotifyManager.s_onInitializeRenderResourcesObserver)
-        Notification.removeObserver(PrefetchGroupManager.s_onFinalizeRenderResourcesObserver)
+        Notification.removeObserver(PrefetchGroupNotifyManager.s_onFinalizeRenderResourcesObserver)
         PrefetchGroupNotifyManager.s_onInitializeRenderResourcesObserver = None
         PrefetchGroupNotifyManager.s_onFinalizeRenderResourcesObserver = None
 
@@ -29,14 +29,10 @@ class PrefetchGroupNotifyManager(object):
                 pass
             elif Prefetch == 1:
                 Mengine.decrementResources(GroupName)
-                pass
             elif Prefetch == 2:
                 Mengine.unfetchResources(GroupName)
-                pass
-            pass
 
         PrefetchGroupNotifyManager.s_groups = []
-        pass
 
     @staticmethod
     def loadParams(module, param):
@@ -48,24 +44,20 @@ class PrefetchGroupNotifyManager(object):
             Tag = record.get("Tag")
 
             PrefetchGroupNotifyManager.s_groups.append((GroupName, Prefetch, Tag))
-            pass
 
         return True
-        pass
 
     @staticmethod
     def __onInitializeRenderResources():
         PrefetchGroupNotifyManager.s_status = PrefetchGroupNotifyManager.STATUS_START
         PrefetchGroupNotifyManager.prefetchGroupsTagged(None)
         return True
-        pass
 
     @staticmethod
     def __onFinalizeRenderResources():
         PrefetchGroupNotifyManager.s_status = PrefetchGroupNotifyManager.STATUS_FINISHED
         PrefetchGroupNotifyManager.unfetchGroupsTagged(None)
         return True
-        pass
 
     @staticmethod
     def mergeGroupsTagged(PrefetchTag, UnfetchTag):
@@ -75,11 +67,8 @@ class PrefetchGroupNotifyManager(object):
         for GroupName, Prefetch, GroupTag in PrefetchGroupNotifyManager.s_groups:
             if GroupTag == PrefetchTag:
                 PrefetchGroups.add(GroupName)
-                pass
             elif GroupTag == UnfetchTag:
                 UnfetchGroups.add(GroupName)
-                pass
-            pass
 
         UnionGroups = PrefetchGroups.union(UnfetchGroups)
         UnfetchGroups.difference(UnionGroups)
@@ -87,12 +76,9 @@ class PrefetchGroupNotifyManager(object):
 
         for GroupName in UnfetchGroups:
             PrefetchGroupNotifyManager.unfetchGroup(GroupName)
-            pass
 
         for GroupName in PrefetchGroups:
             PrefetchGroupNotifyManager.prefetchGroup(GroupName)
-            pass
-        pass
 
     @staticmethod
     def getMergeGroupsTagged(PrefetchTag, UnfetchTag):
@@ -102,18 +88,14 @@ class PrefetchGroupNotifyManager(object):
         for GroupName, Prefetch, GroupTag in PrefetchGroupNotifyManager.s_groups:
             if GroupTag == PrefetchTag:
                 PrefetchGroups.add(GroupName)
-                pass
             elif GroupTag == UnfetchTag:
                 UnfetchGroups.add(GroupName)
-                pass
-            pass
 
         UnionGroups = PrefetchGroups.union(UnfetchGroups)
         UnfetchGroups.difference(UnionGroups)
         PrefetchGroups.difference(UnionGroups)
 
         return UnfetchGroups, PrefetchGroups
-        pass
 
     @staticmethod
     def prefetchGroupsTagged(Tag):
@@ -122,63 +104,45 @@ class PrefetchGroupNotifyManager(object):
         for GroupName, Prefetch, GroupTag in PrefetchGroupNotifyManager.s_groups:
             if GroupTag != Tag:
                 continue
-                pass
 
             if Prefetch == 0:
                 pass
             elif Prefetch == 1:
                 Mengine.incrementResources(GroupName)
-                pass
             elif Prefetch == 2:
                 def __cb(successful, Tag, GroupName):
                     if GroupName in PrefetchGroupNotifyManager.s_prefetch_list[Tag]:
                         PrefetchGroupNotifyManager.s_prefetch_list[Tag].remove(GroupName)
                         Notification.notify(Notificator.onPrefetchGroupsTaggedComplete, Tag, GroupName)
-                        pass
 
                     if len(PrefetchGroupNotifyManager.s_prefetch_list[Tag]) == 0:
                         PrefetchGroupNotifyManager.s_status = PrefetchGroupNotifyManager.STATUS_FINISHED
                         Notification.notify(Notificator.onPrefetchGroupsTaggedFinished, Tag)
-                        pass
-                    pass
 
                 PrefetchGroupNotifyManager.s_prefetch_list[Tag].append(GroupName)
                 if Mengine.prefetchResources(GroupName, __cb, Tag, GroupName) is False:
                     PrefetchGroupNotifyManager.s_prefetch_list[Tag].remove(GroupName)
-                    pass
-                else:
-                    pass
-                pass
-            pass
-        pass
 
     @staticmethod
     def isPrefetchFinished():
         return PrefetchGroupNotifyManager.s_status is PrefetchGroupNotifyManager.STATUS_FINISHED
-        pass
 
     @staticmethod
     def getPrefetchGroup(Tag):
         return PrefetchGroupNotifyManager.s_prefetch_list[Tag]
-        pass
 
     @staticmethod
     def unfetchGroupsTagged(Tag):
         for GroupName, Prefetch, GroupTag in PrefetchGroupNotifyManager.s_groups:
             if GroupTag != Tag:
                 continue
-                pass
 
             if Prefetch == 0:
                 pass
             elif Prefetch == 1:
                 Mengine.decrementResources(GroupName)
-                pass
             elif Prefetch == 2:
                 Mengine.unfetchResources(GroupName)
-                pass
-            pass
-        pass
 
     @staticmethod
     def prefetchGroup(GroupName):
@@ -187,15 +151,11 @@ class PrefetchGroupNotifyManager(object):
                 pass
             elif Prefetch == 1:
                 Mengine.incrementResources(GroupName)
-                pass
             elif Prefetch == 2:
                 def __cb(successful, GroupName):
                     pass
 
                 Mengine.prefetchResources(GroupName, __cb, GroupName)
-                pass
-            pass
-        pass
 
     @staticmethod
     def unfetchGroup(GroupName):
@@ -204,11 +164,5 @@ class PrefetchGroupNotifyManager(object):
                 pass
             elif Prefetch == 1:
                 Mengine.decrementResources(GroupName)
-                pass
             elif Prefetch == 2:
                 Mengine.unfetchResources(GroupName)
-                pass
-            pass
-        pass
-
-    pass
