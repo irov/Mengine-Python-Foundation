2022178f289bedfb56435243d65c314e18cad6ad
eclipse7723 <eclipse7723@gmail.com>
2024-04-11 12:22:28 +0300

    fix animatable play - parent validation

--- Scripts/Foundation/Task/TaskObjectAnimatablePlay.py
+++ Scripts/Foundation/Task/TaskObjectAnimatablePlay.py
@@ -1,6 +1,7 @@
 from Foundation.Task.MixinEvent import MixinEvent
 from Foundation.Task.Task import Task
 
+
 class TaskObjectAnimatablePlay(MixinEvent, Task):
     Skiped = True
 
@@ -13,10 +14,9 @@ class TaskObjectAnimatablePlay(MixinEvent, Task):
         self.Time = params.get("Time", None)
         self.DefaultSpeedFactor = params.get("DefaultSpeedFactor", None)
         self.LastFrame = params.get("LastFrame", True)
-        self.ValidationGroupEnable = params.get("ValidationGroupEnable", True)
+        self.ValidationParentEnable = params.get("ValidationParentEnable", True)
         self.AutoEnable = params.get("AutoEnable", False)
         self.Docdeb = params.get("Docdeb", " Docdeb NOT YOUR OBJECT")
-        pass
 
     def _onValidate(self):
         super(TaskObjectAnimatablePlay, self)._onValidate()
@@ -25,81 +25,65 @@ class TaskObjectAnimatablePlay(MixinEvent, Task):
 
         if Animatable is None:
             self.validateFailed("Animatable is None")
-            pass
+            return
 
         Enable = Animatable.getEnable()
 
         if Enable is False and self.AutoEnable is False:
-            self.validateFailed("Animatable %s is Disable" % (Animatable.getName()))
-            pass
+            self.validateFailed("Animatable '%s' is Disable" % (Animatable.getName()))
 
-        if self.ValidationGroupEnable is True:
-            AnimatableGroup = Animatable.getGroup()
+        if self.ValidationParentEnable is True:
+            AnimatableParent = Animatable.getParent()
 
-            if AnimatableGroup is not None:
-                if AnimatableGroup.getEnable() is False:
-                    self.validateFailed("Animatable %s Group %s is Disable" % (Animatable.getName(), AnimatableGroup.getName()))
-                    pass
-                pass
-            pass
-        pass
+            if AnimatableParent is not None and AnimatableParent.getEnable() is False:
+                self.validateFailed("Animatable '%s' error: Parent '%s' is Disable" % (
+                    Animatable.getName(), AnimatableParent.getName()))
 
     def _onFastSkip(self):
         Animatable = self.getAnimatable()
 
         if self.Loop is not None:
             Animatable.setLoop(self.Loop)
-            pass
 
         if self.Wait is True and self.Loop is None:
             Animatable.setPlay(False)
 
             if self.SpeedFactor is not None:
                 Animatable.setSpeedFactor(self.DefaultSpeedFactor)
-                pass
 
             Animatable.setLastFrame(self.LastFrame)
-            pass
 
         return True
-        pass
 
     def _onRun(self):
         Animatable = self.getAnimatable()
 
         if self.AutoEnable is True:
             Animatable.setEnable(True)
-            pass
 
         if self.StartTiming is not None:
             Animatable.setStartTiming(self.StartTiming)
-            pass
 
         if self.Loop is not None:
             Animatable.setLoop(self.Loop)
-            pass
 
         if self.SpeedFactor is not None:
             if self.DefaultSpeedFactor is None:
                 self.DefaultSpeedFactor = Animatable.getSpeedFactor()
-                pass
 
             TotalSpeedFactor = self.DefaultSpeedFactor * self.SpeedFactor
 
             Animatable.setSpeedFactor(TotalSpeedFactor)
-            pass
         elif self.Time is not None:
             Duration = Animatable.getDuration()
 
             SpeedFactor = Duration / self.Time
             Animatable.setSpeedFactor(SpeedFactor)
-            pass
 
         if self.Wait is False:
             Animatable.setPlay(True)
 
             return True
-            pass
 
         AnimatableEntity = Animatable.getEntity()
 
@@ -107,46 +91,34 @@ class TaskObjectAnimatablePlay(MixinEvent, Task):
             self.log("Animatable '%s' Entity is disable", Animatable.getName())
 
             return True
-            pass
 
         def __onAnimationEndFilter(animatable, isStop):
             if isStop is True:
                 self.setEventSkip(True)
-                pass
 
             if self.SpeedFactor is not None:
                 animatable.setSpeedFactor(self.DefaultSpeedFactor)
-                pass
 
             return True
-            pass
 
         self.addEvent(Animatable.onAnimatableEnd, __onAnimationEndFilter)
 
         Animatable.setPlay(True)
 
         return False
-        pass
 
     def getAnimatable(self):
-        return None
-        pass
+        raise NotImplementedError
 
     def onEndAnimatable(self):
         Animatable = self.getAnimatable()
         self._onEndAnimatable(Animatable)
-        pass
 
     def _onEndAnimatable(self, Animatable):
         pass
 
     def _onEventComplete(self):
         self.onEndAnimatable()
-        pass
-
-    def _onFastSkip(self):
-        return True
-        pass
 
     def _onFinally(self):
         super(TaskObjectAnimatablePlay, self)._onFinally()
@@ -156,20 +128,14 @@ class TaskObjectAnimatablePlay(MixinEvent, Task):
         if Animatable is None:
             self.log("Animatable is None")
             return
-            pass
 
         if self.Wait is True and self.Loop is None:
             Animatable.setPlay(False)
 
             if self.SpeedFactor is not None:
                 Animatable.setSpeedFactor(self.DefaultSpeedFactor)
-                pass
 
             Animatable.setLastFrame(self.LastFrame)
-            pass
 
         if self.AutoEnable is True:
             Animatable.setEnable(False)
-            pass
-        pass
-    pass
