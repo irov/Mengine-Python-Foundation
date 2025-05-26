dc713a6dff41705854c90e07b645c91453e6e29b
Constantin <pshenyanik@gmail.com>
2025-05-26 22:59:39 +0300

    Review changes

--- Scripts/Foundation/LanguagesManager.py
+++ Scripts/Foundation/LanguagesManager.py
@@ -1,6 +1,7 @@
 from Foundation.DatabaseManager import DatabaseManager
 from Foundation.Manager import Manager
 
+
 class LanguagesManager(Manager):
     s_languagesORM = []
 
@@ -14,41 +15,56 @@ class LanguagesManager(Manager):
     def validateTextId(text_id):
         if Mengine.existText(text_id) is False:
             return False
-        return True
 
+        return True
 
     @staticmethod
     def getLanguageTextId(locale):
-        result = DatabaseManager.find(LanguagesManager.s_languagesORM, Language=locale)
-
-        if result is None:
-            Trace.log("Manager", 0, "Locale {!r} not found".format(locale))
+        if Mengine.hasLocale(locale) is False:
             return None
 
-        if LanguagesManager.validateTextId(result.TextId) is False:
-            Trace.log("Manager", 0, "Text id {!r} does not localization".format(result.TextId))
+        language_orm_record = DatabaseManager.find(LanguagesManager.s_languagesORM, Language=locale)
+
+        if language_orm_record is None:
+            Trace.msg("Locale '{!r}' not found".format(locale))
             return None
 
-        return result.TextId
+        if LanguagesManager.validateTextId(language_orm_record.TextId) is False:
+            Trace.msg("Text id '{!r}' for localization '{!r}' not found".format(
+                          language_orm_record.TextId, locale))
+            return None
+
+        return language_orm_record.TextId
 
     @staticmethod
     def getLocale():
         locale = Mengine.getLocale()
+        if locale is None:
+            Trace.msg("Locale '{!r}' is not found!".format(locale))
 
         return locale
 
     @staticmethod
     def setLocale(locale):
+        if Mengine.hasLocale(locale) is False:
+            Trace.msg("Locale '{!r}' is not found!".format(locale))
+            return
+
         Mengine.setLocale(locale)
         Trace.msg("Locale changed to {!r}".format(locale))
 
     @staticmethod
     def getLocales():
         locales = Mengine.getLocales()
+        if locales is None:
+            Trace.msg("There is any Locale not found!")
 
         return locales
 
     @staticmethod
     def hasLocale(locale):
-        if locale is not None:
-            return Mengine.hasLocale(locale)
+        if locale is None:
+            Trace.msg("'hasLocale' got empty argument!")
+            return None
+
+        return Mengine.hasLocale(locale)
