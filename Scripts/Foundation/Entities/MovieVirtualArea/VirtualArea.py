991f122ded78a8027551f6c897e504988ea7bdd3
Constantin <pshenyanik@gmail.com>
2025-06-04 17:53:34 +0300

    Fix drag_object

--- Scripts/Foundation/Entities/MovieVirtualArea/VirtualArea.py
+++ Scripts/Foundation/Entities/MovieVirtualArea/VirtualArea.py
@@ -253,11 +253,13 @@ class VirtualArea(Initializer):
 
         vp = self._target.get_bounds_viewport()
         self._camera.setOrthogonalViewport(vp)
-        self.on_scale(self._target.get_scale_factor())
+        drag_object_scale_factor = self.get_scale_factor()
+        self.on_scale(drag_object_scale_factor)
 
     def set_scale(self, scale):
         """ Scales target and camera to specific `scale` value """
-        scale_factor = scale / self._target.get_scale_factor()
+        drag_object_scale_factor = self.get_scale_factor()
+        scale_factor = scale / drag_object_scale_factor
         self.scale(scale_factor)
 
     def get_scale_factor(self):
